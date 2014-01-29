92e66809d2fed8d91b88658e2885e7b1679e4c22
John Chodera (MSKCC) <choderaj@mskcc.org>
2014-01-28 21:06:10 -0500

    Reporting volume consumed now.

--- pH-array/create-pH-worklist.py
+++ pH-array/create-pH-worklist.py
@@ -40,6 +40,11 @@ compound_volume = 5.0 # compound volume (uL)
 buffer_volume = assay_volume - compound_volume
 assay_RackType = 'Corning 3651'
 
+volume_consumed = dict()
+volume_consumed['compound'] = 0.0
+volume_consumed['citric acid'] = 0.0
+volume_consumed['sodium phosphate'] = 0.0
+
 # Build worklist.
 worklist = ""
 for (condition_index, condition) in enumerate(conditions):
@@ -50,25 +55,35 @@ for (condition_index, condition) in enumerate(conditions):
 
     # compound
     volume = compound_volume
+    volume_consumed['compound'] += volume
     worklist += aspirate('Compound Plate', '4x3 Vial Holder', 1, volume, 1)
     worklist += dispense('Assay Plate', assay_RackType, destination_position, volume, 1)
     worklist += washtips()
 
     # citric acid
     volume = condition['citric acid']*buffer_volume
+    volume_consumed['citric acid'] += volume
     worklist += aspirate('Citric Acid', 'Trough 100ml', 2, volume, 2)
     worklist += dispense('Assay Plate', assay_RackType, destination_position, volume, 2)
     worklist += washtips()
     
     # sodium phosphate
     volume = condition['sodium phosphate']*buffer_volume
+    volume_consumed['sodium phosphate'] += volume
     worklist += aspirate('Sodium Phosphate', 'Trough 100ml', 3, volume, 4)
     worklist += dispense('Assay Plate', assay_RackType, destination_position, volume, 4)
     worklist += washtips()
     
 
+    volume_consumed['sodium phosphate'] += volume
+    
 # Write worklist.
 worklist_filename = 'ph-worklist.gwl'
 outfile = open(worklist_filename, 'w')
 outfile.write(worklist)
 outfile.close()
+
+# Report total volumes.
+print "compound:         %8.3f uL" % volume_consumed['compound']
+print "citric acid:      %8.3f uL" % volume_consumed['citric acid']
+print "sodium phosphate: %8.3f uL" % volume_consumed['sodium phosphate']
